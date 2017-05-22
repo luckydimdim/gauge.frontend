f73d747123301e46b4934a87528cc18d2782b6ff
Dmitriy.Litovchenko <Dmitriy.Litovchenko@yamalspg.ru>
2017-05-22 15:00:28 +0300

    ng 3.1.0 maintenance

--- web/main.dart
+++ web/main.dart
@@ -3,10 +3,14 @@ import 'dart:html';
 import 'package:alert/alert_service.dart';
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2/core.dart';
-import 'package:angular2/src/core/reflection/reflection.dart';
 import 'package:angular2/router.dart';
 import 'package:angular2/platform/common.dart';
+import 'package:aside/aside_service.dart';
+import 'package:config/config_service.dart';
+import 'package:http/http.dart';
+import 'package:http/browser_client.dart';
 
+import 'package:auth/auth_service.dart';
 import 'package:resources_loader/resources_loader.dart';
 import 'package:master_layout/master_layout_component.dart';
 
@@ -59,20 +63,20 @@ class GaugeComponent implements AfterViewInit {
 }
 
 main() async {
-  if (isDebug) {
-    reflector.trackUsage();
-  }
-
   ComponentRef ref = await bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     const Provider(LocationStrategy, useClass: HashLocationStrategy),
     const Provider(ResourcesLoaderService),
-    const Provider(AlertService)
+    const Provider(AlertService),
+    const Provider(AuthenticationService),
+    const Provider(AuthorizationService),
+    const Provider(AsideService),
+    const Provider(ConfigService),
+    provide(Client, useFactory: () => new BrowserClient(), deps: [])
   ]);
 
   if (isDebug) {
     print('Application in DebugMode');
     enableDebugTools(ref);
-    print('Unused keys: ${reflector.listUnusedKeys()}');
   }
 }
